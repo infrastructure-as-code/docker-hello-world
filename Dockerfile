bee9630e2ee7cd83fe377b9f0a8a94499ae5bd1d
Vince Tse <1501612+vincetse@users.noreply.github.com>
2017-11-12 19:19:23 -0500

    Remade in Golang (#1)
    * Update to Golang with Prometheus instrumentation
    
    * Add logging
    
    * Use gin instead of net/http
    
    * Remove unnecessary library
    
    * Instrument gin app
    
    * Add CI/CD
    
    * Remove docker
    
    * Add latest master go
    
    * Add build status
    
    * Cleanup

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,14 @@
-FROM alpine
+# Copied from https://github.com/prometheus/client_golang/blob/master/examples/simple/Dockerfile
 
-RUN apk update && \
-    apk upgrade && \
-    apk add nginx && \
-    mkdir -p /run/nginx
+FROM golang:1.9.2 AS builder
+WORKDIR /go/src/github.com/infrastructure-as-code/docker-hello-world
+ENV GIN_MODE debug
+COPY Makefile *.go ./
+RUN make all
 
-COPY default.conf /etc/nginx/conf.d/default.conf
-EXPOSE 80
-CMD nginx -g "daemon off;"
+FROM scratch
+LABEL maintainer "Vince Tse <thelazyenginerd@gmail.com>"
+COPY --from=builder /go/src/github.com/infrastructure-as-code/docker-hello-world/hello_world .
+ENV GIN_MODE release
+EXPOSE 8080
+ENTRYPOINT ["/hello_world"]
---
+++ hello_world.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"net/http"
+  "os"
+
+  "github.com/gin-gonic/gin"
+  "github.com/zsais/go-gin-prometheus"
+)
+
+var hostname = getHostname()
+
+func getHostname() string {
+  var name, err = os.Hostname()
+  if err != nil {
+    panic(err)
+  }
+  return name
+}
+
+func helloFunc(c *gin.Context) {
+  c.Writer.Header().Set("X-Hostname", hostname)
+  c.String(http.StatusOK, "Hello, World!")
+}
+
+func healthFunc(c *gin.Context) {
+  c.Writer.Header().Set("X-Hostname", hostname)
+  c.String(http.StatusOK, "")
+}
+
+func setupRouter() *gin.Engine {
+  router := gin.Default()
+  ginprom := ginprometheus.NewPrometheus("gin")
+  ginprom.Use(router)
+  router.GET("/", helloFunc)
+  router.GET("/health", healthFunc)
+  return router
+}
+
+func main() {
+  router := setupRouter()
+  router.Run()
+}
