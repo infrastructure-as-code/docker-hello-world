bb6e9ac7e9d931bce25271e4847085b678a04f4c
Vince Tse <vincetse@users.noreply.github.com>
2017-12-02 14:53:57 +0000

    Guilty. Didn't go fmt.

--- hello_world.go
+++ hello_world.go
@@ -2,42 +2,42 @@ package main
 
 import (
 	"net/http"
-  "os"
+	"os"
 
-  "github.com/gin-gonic/gin"
-  "github.com/zsais/go-gin-prometheus"
+	"github.com/gin-gonic/gin"
+	"github.com/zsais/go-gin-prometheus"
 )
 
 var hostname = getHostname()
 
 func getHostname() string {
-  var name, err = os.Hostname()
-  if err != nil {
-    panic(err)
-  }
-  return name
+	var name, err = os.Hostname()
+	if err != nil {
+		panic(err)
+	}
+	return name
 }
 
 func helloFunc(c *gin.Context) {
-  c.Writer.Header().Set("X-Hostname", hostname)
-  c.String(http.StatusOK, "Hello, World!")
+	c.Writer.Header().Set("X-Hostname", hostname)
+	c.String(http.StatusOK, "Hello, World!")
 }
 
 func healthFunc(c *gin.Context) {
-  c.Writer.Header().Set("X-Hostname", hostname)
-  c.String(http.StatusOK, "")
+	c.Writer.Header().Set("X-Hostname", hostname)
+	c.String(http.StatusOK, "")
 }
 
 func setupRouter() *gin.Engine {
-  router := gin.Default()
-  ginprom := ginprometheus.NewPrometheus("gin")
-  ginprom.Use(router)
-  router.GET("/", helloFunc)
-  router.GET("/health", healthFunc)
-  return router
+	router := gin.Default()
+	ginprom := ginprometheus.NewPrometheus("gin")
+	ginprom.Use(router)
+	router.GET("/", helloFunc)
+	router.GET("/health", healthFunc)
+	return router
 }
 
 func main() {
-  router := setupRouter()
-  router.Run()
+	router := setupRouter()
+	router.Run()
 }
--- hello_world_test.go
+++ hello_world_test.go
@@ -1,4 +1,3 @@
-
 package main
 
 import (
@@ -10,33 +9,33 @@ import (
 )
 
 func TestDefaultRoute(t *testing.T) {
-  router := setupRouter()
+	router := setupRouter()
 
 	w := httptest.NewRecorder()
 	req, _ := http.NewRequest("GET", "/", nil)
 	router.ServeHTTP(w, req)
 
-  assert.Equal(t, 200, w.Code)
+	assert.Equal(t, 200, w.Code)
 	assert.Equal(t, "Hello, World!", w.Body.String())
 }
 
 func TestHealthRoute(t *testing.T) {
-  router := setupRouter()
+	router := setupRouter()
 
 	w := httptest.NewRecorder()
 	req, _ := http.NewRequest("GET", "/health", nil)
 	router.ServeHTTP(w, req)
 
-  assert.Equal(t, 200, w.Code)
+	assert.Equal(t, 200, w.Code)
 	assert.Equal(t, "", w.Body.String())
 }
 
 func TestMetricsRoute(t *testing.T) {
-  router := setupRouter()
+	router := setupRouter()
 
 	w := httptest.NewRecorder()
 	req, _ := http.NewRequest("GET", "/metrics", nil)
 	router.ServeHTTP(w, req)
 
-  assert.Equal(t, 200, w.Code)
+	assert.Equal(t, 200, w.Code)
 }
